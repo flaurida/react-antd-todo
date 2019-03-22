87a6759b8dbce9202479dd97ec0fc3fd1cad01cc
leonardopliski <leonardopliski@gmail.com>
2019-03-22 00:49:08 -0300

    Updates

---
+++ precache-manifest.f2204c5cf633803a9f99f84758508c74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "19c4e5aace4c547e68f3",
+    "url": "/react-antd-todo/static/css/main.939bab91.chunk.css"
+  },
+  {
+    "revision": "19c4e5aace4c547e68f3",
+    "url": "/react-antd-todo/static/js/main.809b9cf3.chunk.js"
+  },
+  {
+    "revision": "d95ad0eb903e3fee0e25",
+    "url": "/react-antd-todo/static/js/runtime~main.680813e3.js"
+  },
+  {
+    "revision": "24032421b6cc0eba2e57",
+    "url": "/react-antd-todo/static/css/2.0fe2e8fc.chunk.css"
+  },
+  {
+    "revision": "24032421b6cc0eba2e57",
+    "url": "/react-antd-todo/static/js/2.eb05aff7.chunk.js"
+  },
+  {
+    "revision": "947f15a0dde636b8e7a45fe1b4c1363e",
+    "url": "/react-antd-todo/index.html"
+  }
+];
