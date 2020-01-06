0f1284f163e32de5f85834595796db757d03e9ff
leonardopliski <leonardopliski@gmail.com>
2020-01-06 01:38:53 -0300

    Updates

---
+++ precache-manifest.95841997ae0387a898241dba2d3081ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f253dd528891b1add68657c8e6624c8",
+    "url": "/react-antd-todo/index.html"
+  },
+  {
+    "revision": "2a6b5010e40aa0a7ed22",
+    "url": "/react-antd-todo/static/css/2.fe893b8b.chunk.css"
+  },
+  {
+    "revision": "67060c43ae3565825c65",
+    "url": "/react-antd-todo/static/css/main.e538747f.chunk.css"
+  },
+  {
+    "revision": "2a6b5010e40aa0a7ed22",
+    "url": "/react-antd-todo/static/js/2.8cd48415.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/react-antd-todo/static/js/2.8cd48415.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67060c43ae3565825c65",
+    "url": "/react-antd-todo/static/js/main.7aa4dfb3.chunk.js"
+  },
+  {
+    "revision": "f50f0af9f264eadbce67",
+    "url": "/react-antd-todo/static/js/runtime-main.78e563ea.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-antd-todo/precache-manifest.9f38d76d5af4dbdbe9e0eb86be69a8f6.js"
+  "/react-antd-todo/precache-manifest.95841997ae0387a898241dba2d3081ea.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-antd-todo/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-antd-todo/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
